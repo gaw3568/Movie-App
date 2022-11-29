426f14328cbd597646a4f393312e641c7b2128cb
gaw3568 <gaw3568@gmail.com>
2022-11-29 16:53:13 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const post = "블로그 내용1";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h4>블로그임</h4>
+      </div>
+      <Blog1></Blog1>
+      <h4>{post}</h4>
     </div>
   );
+}
+
+function Blog1() {
+  return (
+    <div className="modal">
+      <h4>title</h4>
+      <p>Date</p>
+      <p>Detail Text</p>
+    </div>
+  )
 }
 
 export default App;
